b10dab0e8a11e6525f19dc0a5ad549ac10cd3b39
Nikhita P Hegde <156395167+NikhitapHegde@users.noreply.github.com>
2024-11-11 10:23:03 +0530

    Create Employee.java

---
+++ 3Employee/Employee.java
@@ -0,0 +1,51 @@
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class Employee {
+    private String id;
+    private String name;
+    private int age;
+    private Date dateOfJoining;
+
+    public Employee(String id, String name, int age, Date dateOfJoining) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.dateOfJoining = dateOfJoining;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Date getDateOfJoining() {
+        return dateOfJoining;
+    }
+
+    @Override
+    public String toString() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        return "Employee{id='" + id + "', name='" + name + "', age=" + age + ", dateOfJoining=" + sdf.format(dateOfJoining) + "}";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Employee employee = (Employee) obj;
+        return id.equals(employee.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
